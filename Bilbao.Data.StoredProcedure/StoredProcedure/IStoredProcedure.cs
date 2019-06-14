3345d6c4ffaae7589453ced68f7d0a5589feb458
JPG-Consulting <opensource@jpg-consulting.es>
2019-06-14 08:28:54 +0200

    Update IStoredProcedure.cs

--- Bilbao.Data.StoredProcedure/StoredProcedure/IStoredProcedure.cs
+++ Bilbao.Data.StoredProcedure/StoredProcedure/IStoredProcedure.cs
@@ -1,7 +1,17 @@
 namespace Bilbao.Data.StoredProcedure
 {
-    public interface IStoredProcedure
+    public interface IStoredProcedure<TResponse, TRequest>
     {
+        DbConnection DbConnection { get; }
 
+        DbTransaction DbTransaction { get; }
+
+        TRequest Parameters { get; }
+
+        IEnumerable<TResponse> ExecuteReader();
+
+        void ExecuteNonQuery();
+
+        
     }
 }
